0ade9bf3a95c76e44c154e63ba88a9b1be946987
treaty1210 <135383414+treaty1210@users.noreply.github.com>
2023-07-10 15:27:52 -0700

    Added event listener to create new object

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import createToDo from "./createToDo";
 import createDefaultProject from "./createDefaultProject";
 import initialDOM from "./initialDOM";
+import createItem from "./addItem";
 
 //create the initial blank project
 createDefaultProject()
@@ -9,5 +10,13 @@ createDefaultProject()
 initialDOM()
 
 //test createToDo
-const toDo1 = new createToDo('Work on ToDo List', 'Create a ToDo list that is functional.', 'TBD', 'High')
-console.log('properties for toDo1', toDo1)
+// const toDo1 = new createToDo('Work on ToDo List', 'Create a ToDo list that is functional.', 'TBD', 'High')
+// console.log('properties for toDo1', toDo1)
+
+//Button to add filled out task to display
+document.querySelector("#createItem").addEventListener("click", (e) => {
+    e.preventDefault();
+    createItem();
+    let newItem = new createToDo(title, description, dueDate, priority)
+    console.log(newItem)
+})
