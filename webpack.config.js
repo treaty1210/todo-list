60a335f5bb22ddb479d5c8f661ebd274e6efe32b
treaty1210 <135383414+treaty1210@users.noreply.github.com>
2023-07-10 11:54:17 -0700

    Added mode type

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
