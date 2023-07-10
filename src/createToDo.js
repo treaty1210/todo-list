66950cc5e453653e02983c2a43e18085e0949418
treaty1210 <135383414+treaty1210@users.noreply.github.com>
2023-07-10 12:31:33 -0700

    Created factory function for tasks

---
+++ src/createToDo.js
@@ -0,0 +1,8 @@
+function createToDo(title, description, dueDate, priority) {
+    this.title = title
+    this.description = description
+    this.dueDate = dueDate
+    this.priority = priority
+}
+
+export default createToDo;
