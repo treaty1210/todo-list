7f545323c06a71d343149c70c5e9bbcdd9b6a72a
treaty1210 <135383414+treaty1210@users.noreply.github.com>
2023-07-12 12:33:17 -0700

    Set up local storage
    Also runs displayProject

--- src/saveLocal.js
+++ src/saveLocal.js
@@ -1,7 +1,12 @@
+import displayProject from "./displayProject"
+
 function saveLocal(title, description, dueDate, priority) {
     localStorage.setItem('title', document.getElementById('title').value)
     localStorage.setItem('description', document.getElementById('description').value)
     localStorage.setItem('dueDate', document.getElementById('dueDate').value)
     localStorage.setItem('priority', document.getElementById('priority').value)
+    displayProject()
     return { title, description, dueDate, priority }
 }
+
+export default saveLocal
